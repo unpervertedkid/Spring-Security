16c83c677b78a76f4b6c44372ec01dad38946316
Brian Silah <unpervertedkid@gmail.com>
2023-03-15 13:07:35 +0300

    Commented out the schemas and data for user security details to use ldap

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,18 +1,26 @@
 INSERT INTO customers (name, contact_name, email, phone) values ('Acme', 'Wylie Coyote', 'wcoyote@acme.com', '1-515-555-2348');
 INSERT INTO customers (name, contact_name, email, phone) values ('Spacely Space Sprockets', 'George Jettson', 'gjettson@spacely.com', '1-515-555-2350');
-INSERT INTO customers (name, contact_name, email, phone) values ('Callahan Auto', 'Thomas Callhan', 'tcallahan@callhhanauto.com', '1-515-555-2333');
-INSERT INTO customers (name, contact_name, email, phone) values ('Dundler Mifflin Inc', 'Michael Scott', 'mscott@dundlermifflin.com', '1-515-555-2320');
-INSERT INTO customers (name, contact_name, email, phone) values ('Stark Industries', 'Tony Stark', 'tstark@stark.com', '1-515-555-7777');
-INSERT INTO customers (name, contact_name, email, phone) values ('Initech', 'Peter Gibbons', 'pgibbons@initec.com', '1-515-555-0666');
-INSERT INTO customers (name, contact_name, email, phone) values ('Wayne Enterprises', 'Bruce Wayne', 'bwayne@wayne.com', '1-515-555-1111');
+INSERT INTO customers (name, contact_name, email, phone)
+values ('Callahan Auto', 'Thomas Callhan', 'tcallahan@callhhanauto.com', '1-515-555-2333');
+INSERT INTO customers (name, contact_name, email, phone)
+values ('Dundler Mifflin Inc', 'Michael Scott', 'mscott@dundlermifflin.com', '1-515-555-2320');
+INSERT INTO customers (name, contact_name, email, phone)
+values ('Stark Industries', 'Tony Stark', 'tstark@stark.com', '1-515-555-7777');
+INSERT INTO customers (name, contact_name, email, phone)
+values ('Initech', 'Peter Gibbons', 'pgibbons@initec.com', '1-515-555-0666');
+INSERT INTO customers (name, contact_name, email, phone)
+values ('Wayne Enterprises', 'Bruce Wayne', 'bwayne@wayne.com', '1-515-555-1111');
 
-INSERT INTO orders (customer_id, order_info) values ((SELECT customer_id FROM customers where name = 'Acme'), '1500 Widgets');
-INSERT INTO orders (customer_id, order_info) values ((SELECT customer_id FROM customers where name = 'Acme'), '3000 Widgets');
-INSERT INTO orders (customer_id, order_info) values ((SELECT customer_id FROM customers where name = 'Callahan Auto'), '200 Widgets');
+INSERT INTO orders (customer_id, order_info)
+values ((SELECT customer_id FROM customers where name = 'Acme'), '1500 Widgets');
+INSERT INTO orders (customer_id, order_info)
+values ((SELECT customer_id FROM customers where name = 'Acme'), '3000 Widgets');
+INSERT INTO orders (customer_id, order_info)
+values ((SELECT customer_id FROM customers where name = 'Callahan Auto'), '200 Widgets');
 
-INSERT INTO users (username, password, enabled) values ('user', '$2a$12$lhKYm9qlGUWGlv1k2.I6ueO3mMHKjmV0TOzv/uCnxVGVlZzoR.owC', true);
-INSERT INTO users (username, password, enabled) values ('admin', '$2a$12$lhKYm9qlGUWGlv1k2.I6ueO3mMHKjmV0TOzv/uCnxVGVlZzoR.owC', true);
-
-INSERT INTO authorities (username, authority) values ('user', 'ROLE_USER');
-INSERT INTO authorities (username, authority) values ('admin', 'ROLE_USER');
-INSERT INTO authorities (username, authority) values ('admin', 'ROLE_ADMIN');
+-- INSERT INTO users (username, password, enabled) values ('user', '$2a$12$lhKYm9qlGUWGlv1k2.I6ueO3mMHKjmV0TOzv/uCnxVGVlZzoR.owC', true);
+-- INSERT INTO users (username, password, enabled) values ('admin', '$2a$12$lhKYm9qlGUWGlv1k2.I6ueO3mMHKjmV0TOzv/uCnxVGVlZzoR.owC', true);
+--
+-- INSERT INTO authorities (username, authority) values ('user', 'ROLE_USER');
+-- INSERT INTO authorities (username, authority) values ('admin', 'ROLE_USER');
+-- INSERT INTO authorities (username, authority) values ('admin', 'ROLE_ADMIN');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,30 +1,31 @@
 CREATE TABLE customers(
-    customer_id bigint auto_increment primary key,
-    name varchar(64) not null,
-    contact_name varchar(128) not null,
-    email varchar(128) not null,
-    phone varchar(24) not null
+                          customer_id  bigint auto_increment primary key,
+                          name         varchar(64)  not null,
+                          contact_name varchar(128) not null,
+                          email        varchar(128) not null,
+                          phone        varchar(24)  not null
 );
 
-CREATE TABLE orders(
-    order_id bigint auto_increment primary key,
-    customer_id bigint not null,
-    order_info varchar (2048) not null,
-    foreign key (customer_id) references customers(customer_id)
-);
-
-CREATE TABLE users
+CREATE TABLE orders
 (
-    username varchar(50) not null primary key,
-    password varchar(500) not null,
-    enabled  boolean     not null
+    order_id    bigint auto_increment primary key,
+    customer_id bigint        not null,
+    order_info  varchar(2048) not null,
+    foreign key (customer_id) references customers (customer_id)
 );
 
-CREATE TABLE authorities
-(
-    username varchar_ignorecase(50) not null,
-    authority varchar_ignorecase(50) not null,
-    constraint fk_authorities_users foreign key (username) references users (username)
-);
-
-CREATE UNIQUE INDEX ix_auth_username on authorities (username,authority);
+-- CREATE TABLE users
+-- (
+--     username varchar(50) not null primary key,
+--     password varchar(500) not null,
+--     enabled  boolean     not null
+-- );
+--
+-- CREATE TABLE authorities
+-- (
+--     username varchar_ignorecase(50) not null,
+--     authority varchar_ignorecase(50) not null,
+--     constraint fk_authorities_users foreign key (username) references users (username)
+-- );
+--
+-- CREATE UNIQUE INDEX ix_auth_username on authorities (username,authority);
